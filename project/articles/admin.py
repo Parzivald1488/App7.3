ef2b55107eda25cd453e4aadaae189d7f565859a
Parzivald <parzivald@inbox.ru>
2022-09-25 22:32:18 +0500

    7.7 FFFFFFu...

--- project/articles/admin.py
+++ project/articles/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Article
 
-admin.site.register(Article)
+from . import models
+
+
+class CommentInLine(admin.TabularInline):
+    models = models.Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
--- project/articles/models.py
+++ project/articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, )
 
     def __str__(self):
         return self.title
@@ -15,4 +15,18 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
 
-# Create your models here.
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_list')
